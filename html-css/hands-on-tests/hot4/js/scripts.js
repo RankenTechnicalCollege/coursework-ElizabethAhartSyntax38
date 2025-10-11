3ecc7b875a7a07b2fb58cbf80d180c0339dd8ab6
ElizabethAhartSyntax38 <elizabeth_ahart@insideranken.org>
2025-10-11 11:37:20 -0500

    hot3 completed

--- html-css/hands-on-tests/hot4/js/scripts.js
+++ html-css/hands-on-tests/hot4/js/scripts.js
@@ -1,5 +1,18 @@
 //elizabeth Ahart 10-8-25 HOT$
 
-$(document).ready(collapse(){
-    var ACC
-})
+$(document).ready(function collapse(){
+
+ var menu = document.getElementsByClassName("accordion-body");
+ var link = document.getElementsByClassName("accordionFlushExample");
+
+ if(menu.style.display==="none" && link.style.display ==="none"){
+  menu.style.display="block";
+ link.style.display="block";
+ }else{
+  menu.style.display="none";
+  link.style.display="none";
+ }
+ 
+ 
+});
+
