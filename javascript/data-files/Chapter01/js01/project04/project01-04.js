2acf487fbfdb7a8b73f50efb41fcb46292abfb3f
ElizabethAhartSyntax38 <elizabeth_ahart@insideranken.org>
2025-10-19 19:28:33 -0500

    js01
    js01 project 1-5

--- javascript/data-files/Chapter01/js01/project04/project01-04.js
+++ javascript/data-files/Chapter01/js01/project04/project01-04.js
@@ -9,4 +9,6 @@
 */
 
 //define variables for home and work addresses
+var homeStreet ="1 Main St", homeCity = "Sicilia", homeState = "MA", homeCode = "02103", workStreet= "15 Oak Ln.", workCity = "Central City", workState = "MA", workCode ="02104";
+
 
