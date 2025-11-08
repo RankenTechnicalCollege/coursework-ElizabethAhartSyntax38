39016bc1b428c493925495f23d644c5bf2a1b4c8
ElizabethAhartSyntax38 <elizabeth_ahart@insideranken.org>
2025-11-08 15:35:35 -0600

    ch5 java almost complete

--- javascript/data-files/Chapter05/js05/chapter/js05.js
+++ javascript/data-files/Chapter05/js05/chapter/js05.js
@@ -9,6 +9,46 @@
 
       Filename: js05.js
 */
+window.addEventListener("load", createLightbox);
+function createLightbox(){
+   //Lightbox container
+   let lightBox = document.getElementById("lightBox");
+
+   //parts of the lightbox
+   let lbTitle = document.createElement("h1");
+   let lbCounter = document.createElement("div");
+   let lbPrev = document.createElement("div");
+   let lbNext = document.createElement("div");
+   let lbPlay = document.createElement("div");
+   let lbImages = document.createElement("div");
+
+   //Design the lightbox title
+   lightBox.appendChild(lbTitle);
+   lbTitle.id="lbTitle";
+   //design the slide counter
+   lightBox.appendChild(lbCounter);
+   lbCounter.id="lbCounter"
+   //design the lightbox prev slide button
+   lightBox.appendChild(lbPrev);
+   lbPrev.id+"lbPrev";
+   //design the lightbox next button
+   lightBox.appendChild(lbNext);
+   lbNext.id="lbNext";
+   //design the light hous play pause button
+   lightBox.appendChild(lbPlay);
+   lbPlay.id="lbPlay"
+   //design the lightbox images container
+   lightBox.appendChild(lbImages);
+   lbImages.id="lbImages";
+
+   //add images from the imgFiles array to the container
+   for(let i=0;i<imgCount;i++){
+      let image =document.createElement("img");
+       image.src=imgFiles[i];
+       image.alt=imgCaptions[i];
+       lbImages.appendChild(image);
+   }
+}
 
 window.addEventListener("load", setupGallery);
 
